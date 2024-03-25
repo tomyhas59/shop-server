691de072563db718ef639edac83be8bbd2fb7a1c
이용현 <yh9035926@naver.com>
2024-03-25 09:21:48 +0900

    cursor

--- src/resolvers/product.ts
+++ src/resolvers/product.ts
@@ -2,8 +2,10 @@ import { Resolvers } from "./types";
 
 const productResolver: Resolvers = {
   Query: {
-    products: (parent, args, context) => {
-      return context.db.products;
+    products: (parent, /**args */ { cursor = "" }, context) => {
+      const fromIndex =
+        context.db.products.findIndex((product) => product.id === cursor) + 1;
+      return context.db.products.slice(fromIndex, fromIndex + 15) || [];
     },
     product: (parent, { id }, { db }) => {
       const found = db.products.find((item) => item.id === id);
