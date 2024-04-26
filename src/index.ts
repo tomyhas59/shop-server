1c422402709dd5c861a2e10e195784e61a27dd1e
yh9035926 <yh9035926@naver.com>
2024-04-27 00:57:00 +0900

    cors 설치

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,38 @@
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
-import schema from "./schema";
-import resolvers from "./resolvers";
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const schema = require("./schema");
+const resolvers = require("./resolvers");
+const cors = require("cors");
+
+const isProduction = process.env.NODE_ENV === "production";
+
+const server = new ApolloServer({
+  typeDefs: schema,
+  resolvers,
+});
+
+const app = express();
+
+// Enable CORS middleware
+app.use(
+  cors({
+    origin: isProduction
+      ? ["https://tmshop.vercel.app", "https://studio.apollographql.com"]
+      : ["http://localhost:3000", "https://studio.apollographql.com"],
+    credentials: true,
+  })
+);
 
 (async () => {
-  const isProduction = process.env.NODE_ENV === "production";
-
-  const server = new ApolloServer({
-    typeDefs: schema,
-    resolvers,
-  });
-
-  const app: any = express();
   await server.start();
+
   server.applyMiddleware({
     app,
     path: "/graphql",
-    cors: {
-      origin: isProduction
-        ? ["https://tmshop.vercel.app", "https://studio.apollographql.com"]
-        : ["http://localhost:3000", "https://studio.apollographql.com"], //프론트 url
-      credentials: true,
-    },
   });
-  const port = isProduction ? 8000 : 7000;
-  await app.listen({ port: port });
 
-  console.log("server 실행 중", port);
+  const port = isProduction ? 8000 : 7000;
+  app.listen({ port }, () => {
+    console.log("Server running at port", port);
+  });
 })();
